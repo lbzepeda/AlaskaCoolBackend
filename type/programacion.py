39c59e23c83f859390d6301229af73add99b6d1c
m272wsakoqfrw4ev2ilukcldmjkjdivxlwdadrlbpyyzyx4lx5ga <reko.image>
2023-07-30 15:20:24 -0600

    update requirement

--- type/programacion.py
+++ type/programacion.py
@@ -1,10 +1,11 @@
 import typing
 import strawberry
 from conn.db import conn
-from models.index import programacion, productos, proforma, det_proforma, usuarios, cuadrillas, usuario_cuadrilla
+from models.index import programacion, productos, proforma, det_proforma, usuarios, cuadrillas, usuario_cuadrilla, facturas, det_facturas
 from strawberry.types import Info
 from datetime import datetime
 from typing import Optional
+from decimal import Decimal
 
 lstProductos = conn.execute(productos.select()).fetchall()
 lstProforma = conn.execute(proforma.select()).fetchall()
@@ -12,6 +13,8 @@ lstDetProformas = conn.execute(det_proforma.select()).fetchall()
 lstUsuarios = conn.execute(usuarios.select()).fetchall()
 lstCuadrillas = conn.execute(cuadrillas.select()).fetchall()
 lstUsuario_Cuadrilla = conn.execute(usuario_cuadrilla.select()).fetchall()
+lstFacturas = conn.execute(facturas.select()).fetchall()
+lstDetFacturas = conn.execute(det_facturas.select()).fetchall()
 
 @strawberry.type
 class Productos:
@@ -35,7 +38,7 @@ class Productos:
     Gravable: str
     PorcComision: float
     Consignacion: str
-    Moneda: str
+    Moneda: Optional[str] = None
     CodDescuento: str
     CodTipoProducto: str
     CodUbicacion: str
@@ -297,6 +300,179 @@ class Cuadrilla:
     descripcion: str
 
 @strawberry.type
+class DetalleFactura:
+    Sucursal: str
+    NoFactura: str
+    Serie: str
+    Tipo: str
+    Modo: str
+    FechaFactura: datetime
+    Producto: str
+    @strawberry.field
+    def producto(self, info: Info) -> typing.List[Optional[Productos]]:  
+        productos = [Productos(**dict(producto._mapping)) for producto in lstProductos if producto.CodProducto == self.Producto]
+        return productos if productos else []
+    UMedida: str
+    Cantidad: Optional[Decimal]
+    Bonificacion: Decimal
+    Financiamiento: Decimal
+    Precio: Optional[Decimal]
+    Porcen_Descto: Optional[Decimal]
+    Descuento: Optional[Decimal]
+    Iva: Optional[Decimal]
+    Costo: Decimal
+    Exonerado: Optional[Decimal]
+    Bod_Descargue: str
+    CodPrecio: str
+    Registro_Usuario: Optional[str]
+    Registro_Maquina: Optional[str]
+    Registro_Fecha: Optional[datetime]
+    nombre_producto: Optional[str]
+    Tipodesc: Optional[str]
+    tipofac: str
+    Numero: int
+    valor: Optional[Decimal]
+    cod_combo: str
+    orden: int
+    indice: int
+    Porcentaje_ComisionLista: Decimal
+    TipoComponente: str
+    Gravado: str
+    MedidaCosteo: str
+    MedidaMovimiento: str
+    CantidadMovimiento: Optional[Decimal]
+    Medida_Bonificacion: str
+    Bonificacion_Movimiento: Optional[Decimal]
+    saldo: Decimal
+    PrecioDeLista: Decimal
+    DescuentoFijo: str
+    PrioridadDescuento: int
+    TipoServicio: int
+    Cupones: int
+    Lote: str
+    Vencimiento: datetime
+    Receta: str
+    Ruta: int
+    IdProgramacion: int
+    RegistroFechaApp: datetime
+    num_parte: str
+    OrdenServicio: str
+    CentroCosto2: int
+    CentroCosto3: int
+    Impuesto1: Optional[Decimal]
+    Impuesto2: Optional[Decimal]
+    Impuesto3: Optional[Decimal]
+    Impuesto4: Optional[Decimal]
+    Impuesto5: Optional[Decimal]
+    ProductoConsignado: str
+    NumeroTrasladoConsignado: Optional[str]
+
+@strawberry.type
+class Factura:
+    NoFactura: str
+    @strawberry.field
+    def det_factura(self, info: Info) -> typing.List[Optional[DetalleFactura]]:  
+        detFacturas = [DetalleFactura(**dict(detFactura._mapping)) for detFactura in lstDetFacturas if detFactura.NoFactura == self.NoFactura]
+        return detFacturas if detFacturas else []
+    Serie: str
+    Tipo: str
+    Modo: str
+    FechaFactura: datetime
+    Moneda: str
+    Efectivo: float
+    Tarjeta: float
+    Cheque: float
+    RetencionIR: Optional[float] = None
+    RetencionALMA: Optional[float] = None
+    SubTotal: Optional[float] = None
+    Descuento: Optional[float] = None
+    Iva: Optional[float] = None
+    MontoExonerado: float
+    TotalPago: Optional[float] = None
+    CodListaPrecio: str
+    Sucursal: str
+    CodCli: str
+    Nombrede: str
+    Ruta: str
+    Rutero: str
+    CentroVenta: str
+    Supervisor: str
+    Colector: str
+    Anulado: str
+    valtas: float
+    Fecha_Vence: datetime
+    RegistroFecha: datetime
+    RegistroUsuario: str
+    RegistroMaquina: str
+    Exonerado: str
+    ReferenciaExoneracion: str
+    Consignatario: str
+    Plazo: float
+    FormaPago: str
+    MontoPrima: float
+    FechaPagoPrima: datetime
+    Cuotas: int
+    Financiamiento: float
+    TipoCasa: str
+    TiempoResidir: str
+    CodTipoCliente: str
+    NoIdentificacion: str
+    clientecontado: str
+    revertida: str
+    NumOrden: str
+    numprecierre: str
+    imprimir: str
+    numero: int
+    conteo: int
+    noapartado: str
+    caja: str
+    numcierre: Optional[str]
+    NoRecerva: str
+    Version: str
+    NoTransaccionInv: int
+    CodClienteSucursal: int
+    Cajero: str
+    Observacion: str
+    VerComoPreventa: str
+    TipoServicio: int
+    Transporte: float
+    Propina: float
+    Agente: int
+    NombreSubCliente: str
+    ClientePadre: str
+    NumeroExoneracion: str
+    IdentificadorPreventa: int
+    ReferenciaPreventa: int
+    IdEstado: int
+    FechaRetorno: datetime
+    MotivoRevertida: str
+    HoraRevertida: datetime
+    HoraEnviado: datetime
+    CantidadCupones: int
+    Referencia: str
+    NoPedido: str
+    EtiquetaInformativa: str
+    Longitud: float
+    Latitud: float
+    IdProgramacion: int
+    AplicaGarantia: str
+    RegistroFechaApp: datetime
+    Impuesto1: Optional[float]
+    Impuesto2: Optional[float]
+    Impuesto3: Optional[float]
+    Impuesto4: Optional[float]
+    Impuesto5: Optional[float]
+    NumeroTrasladoConsignado: str
+    BodDestino: str
+    BodDestinoConsignacion: str
+    NoConsignacionCLiente: str
+    PlanDePago: str
+    ROCPRIMA: str
+    NoPlanPago: int
+    ValorInteres: float
+    IdPromocion: str
+
+@strawberry.type
 class UsuarioCuadrilla:
     id: int
     idUsuario: int
@@ -323,11 +499,15 @@ class Programacion:
         return productos if productos else []
     codcliente: Optional[str] = None
     codfactura: Optional[str] = None
+    @strawberry.field
+    def factura(self, info: Info) -> Optional[Factura]:  
+        facturas = [Factura(**dict(factura._mapping)) for factura in lstFacturas if factura and factura.NoFactura == self.codfactura]
+        return facturas[0] if facturas else None
     codproforma: Optional[str] = None
     @strawberry.field
-    def proforma(self, info: Info) -> typing.List[Optional[Proforma]]:  
-        proformas = [Proforma(**dict(proforma._mapping)) for proforma in lstProforma if proforma.NoFactura == self.codproforma]
-        return proformas if proformas else []
+    def proforma(self, info: Info) -> Optional[Proforma]:  
+        proformas = [Proforma(**dict(proforma._mapping)) for proforma in lstProforma if proforma and proforma.NoFactura == self.codproforma]
+        return proformas[0] if proformas else None
     idUsuarioCreacion: int
     idCuadrilla: Optional[int] = None
     @strawberry.field
