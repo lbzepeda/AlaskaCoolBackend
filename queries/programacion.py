72743ecb9a1819f1e451c22bca3723d2a87644ef
m272wsakoqfrw4ev2ilukcldmjkjdivxlwdadrlbpyyzyx4lx5ga <reko.image>
2023-08-15 19:24:46 -0600

    update requirement

--- queries/programacion.py
+++ queries/programacion.py
@@ -13,65 +13,57 @@ from .departamentos import Departamentos
 from .estadoprogramacion import EstadoProgramacion
 from .usuario import Usuario
 
-lstProductos = conn.execute(productos.select()).fetchall()
-lstProforma = conn.execute(proforma.select()).fetchall()
-lstDetProformas = conn.execute(det_proforma.select()).fetchall()
-lstUsuarios = conn.execute(usuarios.select()).fetchall()
-lstCuadrillas = conn.execute(cuadrillas.select()).fetchall()
-lstUsuario_Cuadrilla = conn.execute(usuario_cuadrilla.select()).fetchall()
-lstFacturas = conn.execute(facturas.select()).fetchall()
-lstDetFacturas = conn.execute(det_facturas.select()).fetchall()
-lstHorarioProgramacion = conn.execute(horario_programacion.select()).fetchall()
-lstDepartamentos = conn.execute(departamentos.select()).fetchall()
-lstEstadoProgramacion = conn.execute(estado_programacion.select()).fetchall()
-
 @strawberry.type
 class Programacion:
     id: int
     codservicio: str
     @strawberry.field
-    def servicio(self, info: Info) -> typing.List[Optional[Productos]]:  
-        productos = [Productos(**dict(producto._mapping)) for producto in lstProductos if producto.CodProducto == self.codservicio]
-        return productos if productos else []
+    def servicio(self, info: Info) -> typing.List[Optional[Productos]]:
+        current_productos = conn.execute(productos.select()).fetchall()
+        matching_products = [Productos(**dict(producto._mapping)) for producto in current_productos if producto.CodProducto == self.codservicio]
+        return matching_products or []
     codcliente: Optional[str] = None
     codfactura: Optional[str] = None
     @strawberry.field
-    def factura(self, info: Info) -> Optional[Factura]:  
-        facturas = [Factura(**dict(factura._mapping)) for factura in lstFacturas if factura and factura.NoFactura == self.codfactura]
-        return facturas[0] if facturas else None
+    def factura(self, info: Info) -> Optional[Factura]:
+        current_facturas = conn.execute(facturas.select()).fetchall()
+        matching_facturas = [Factura(**dict(factura._mapping)) for factura in current_facturas if factura.NoFactura == self.codfactura]
+        return matching_facturas[0] if matching_facturas else None
     codproforma: Optional[str] = None
     @strawberry.field
-    def proforma(self, info: Info) -> Optional[Proforma]:  
-        proformas = [Proforma(**dict(proforma._mapping)) for proforma in lstProforma if proforma and proforma.NoFactura == self.codproforma]
-        return proformas[0] if proformas else None
+    def proforma(self, info: Info) -> Optional[Proforma]:
+        current_proformas = conn.execute(proforma.select()).fetchall()
+        matching_proformas = [Proforma(**dict(proforma._mapping)) for proforma in current_proformas if proforma.NoFactura == self.codproforma]
+        return matching_proformas[0] if matching_proformas else None
     idUsuarioCreacion: int
     @strawberry.field
-    def usuario_creacion(self, info: Info) -> Optional[Usuario]:  
-        usuariocreador = [Usuario(**dict(usuariocreador._mapping)) for usuariocreador in lstUsuarios if usuariocreador and usuariocreador.id == self.idUsuarioCreacion]
-        return usuariocreador[0] if usuariocreador else None
+    def usuario_creacion(self, info: Info) -> Optional[Usuario]:
+        usuario = conn.execute(usuarios.select().where(usuarios.c.id == self.idUsuarioCreacion)).first()
+        return Usuario(**dict(usuario._mapping)) if usuario else None
     idCuadrilla: Optional[int] = None
     @strawberry.field
-    def cuadrilla(self, info: Info) -> typing.List[Optional[UsuarioCuadrilla]]:  
-        usuariocuadrilla = [UsuarioCuadrilla(**dict(usuario_cuadrilla._mapping)) for usuario_cuadrilla in lstUsuario_Cuadrilla if usuario_cuadrilla.idCuadrilla == self.idCuadrilla]
-        return usuariocuadrilla if usuariocuadrilla else []
+    def cuadrilla(self, info: Info) -> typing.List[Optional[UsuarioCuadrilla]]:
+        cuadrillas = conn.execute(usuario_cuadrilla.select().where(usuario_cuadrilla.c.idCuadrilla == self.idCuadrilla)).fetchall()
+        return [UsuarioCuadrilla(**dict(cuadrilla._mapping)) for cuadrilla in cuadrillas] or []
+    
     idHorarioProgramacion: Optional[int] = None
     @strawberry.field
-    def horarioprogramacion(self, info: Info) -> Optional[HorarioProgramacion]:  
-        horarioprogramacion = [HorarioProgramacion(**dict(horarioprogramacion._mapping)) for horarioprogramacion in lstHorarioProgramacion if horarioprogramacion and horarioprogramacion.id == self.idHorarioProgramacion]
-        return horarioprogramacion[0] if horarioprogramacion else None
+    def horarioprogramacion(self, info: Info) -> Optional[HorarioProgramacion]:
+        horario = conn.execute(horario_programacion.select().where(horario_programacion.c.id == self.idHorarioProgramacion)).first()
+        return HorarioProgramacion(**dict(horario._mapping)) if horario else None
     UrlGeoLocalizacion: str
     direccion: Optional[str]
     observaciones: Optional[str]
     idDepartamento: int
     @strawberry.field
-    def departamento(self, info: Info) -> Optional[Departamentos]:  
-        departamento = [Departamentos(**dict(departamento._mapping)) for departamento in lstDepartamentos if departamento and departamento.id == self.idDepartamento]
-        return departamento[0] if departamento else None
+    def departamento(self, info: Info) -> Optional[Departamentos]:
+        departamento = conn.execute(departamentos.select().where(departamentos.c.id == self.idDepartamento)).first()
+        return Departamentos(**dict(departamento._mapping)) if departamento else None
     idEstadoProgramacion: int
     @strawberry.field
-    def estado_programacion(self, info: Info) -> Optional[EstadoProgramacion]:  
-        estadoprogramacion = [EstadoProgramacion(**dict(estadoprogramacion._mapping)) for estadoprogramacion in lstEstadoProgramacion if estadoprogramacion and estadoprogramacion.id == self.idEstadoProgramacion]
-        return estadoprogramacion[0] if estadoprogramacion else None
+    def estado_programacion(self, info: Info) -> Optional[EstadoProgramacion]:
+        estado = conn.execute(estado_programacion.select().where(estado_programacion.c.id == self.idEstadoProgramacion)).first()
+        return EstadoProgramacion(**dict(estado._mapping)) if estado else None
     @classmethod
     def from_row(cls, row):
         return cls(**row)
