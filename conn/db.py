6fa503d74effd35c0e1ab3df45cc39ffe04b94af
m272wsakoqfrw4ev2ilukcldmjkjdivxlwdadrlbpyyzyx4lx5ga <reko.image>
2023-08-15 19:55:48 -0600

    update requirement

--- conn/db.py
+++ conn/db.py
@@ -1,5 +1,9 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine('mysql+pymysql://root:Carl1991*+1@34.173.53.131:3306/alaskacool', echo=True, query_cache_size=0)
+engine = create_engine('mysql+pymysql://root:Carl1991*+1@34.173.53.131:3306/alaskacool', echo=True,
+    echo_pool=True,
+    pool_use_lifo=True,
+    pool_pre_ping=True,
+    pool_recycle=3600)
 meta = MetaData()
 conn = engine.connect()
