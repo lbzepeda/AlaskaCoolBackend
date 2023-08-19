3031d85580245c8f996d7c5e04d2afcf9b3c1176
m272wsakoqfrw4ev2ilukcldmjkjdivxlwdadrlbpyyzyx4lx5ga <reko.image>
2023-08-18 18:11:59 -0600

    update requirement

--- mutations/archivoprogramacion.py
+++ mutations/archivoprogramacion.py
@@ -1,13 +1,38 @@
+import typing
+import os
 import strawberry
+from strawberry.file_uploads import Upload
 from conn.db import conn
 from models.index import archivo_programacion
 from strawberry.types import Info
+
+SAVE_PATH = "files/programation/"
+
+@strawberry.mutation
+async def cargar_archivo_programacion(self, file: Upload, idTipoArchivo: int, idProgramacion: int) -> int:
+    # Crear un nombre de archivo seguro usando el id
+    filename = os.path.join(SAVE_PATH, f"{idProgramacion}_{file.filename}")
+
+    # Guardar el archivo en el sistema de archivos
+    with open(filename, 'wb') as buffer:
+        content = await file.read()
+        buffer.write(content)
+
+    # Guardar la ruta del archivo en la base de datos
+    archivoprogramacion = {
+        "PathArchivo": filename,
+        "idTipoArchivo": idTipoArchivo,
+        "idProgramacion": idProgramacion
+    }
+    result = conn.execute(archivo_programacion.insert(), archivoprogramacion)
+    conn.commit()
+    return int(result.inserted_primary_key[0])
 
 @strawberry.mutation
 async def crear_archivo_programacion(self, PathArchivo: str, idTipoArchivo: int, idProgramacion:int, info: Info) -> int:
     archivoprogramacion =  {
         "PathArchivo": PathArchivo,
-        "≈": idTipoArchivo,
+        "idTipoArchivo": idTipoArchivo,
         "idProgramacion": idProgramacion
     }
     result = conn.execute(archivo_programacion.insert(),archivoprogramacion)
@@ -24,4 +49,4 @@ def actualizar_archivo_programacion(self, id:int, PathArchivo: str, idTipoArchiv
     conn.commit();
     return str(result.rowcount) + " Row(s) updated"
 
-lstArchivoProgramacionMutation = [crear_archivo_programacion, actualizar_archivo_programacion]
+lstArchivoProgramacionMutation = [crear_archivo_programacion, actualizar_archivo_programacion, cargar_archivo_programacion]
