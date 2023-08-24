d1abb1809e023105ee3a7415d60a9d24b3d2e517
m272wsakoqfrw4ev2ilukcldmjkjdivxlwdadrlbpyyzyx4lx5ga <reko.image>
2023-08-24 16:13:18 -0600

    update requirement

--- mutations/programacion.py
+++ mutations/programacion.py
@@ -15,6 +15,9 @@ from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from datetime import timedelta
 from enum import Enum
+from json import dumps
+from httplib2 import Http
+
 
 class TipoUsuario(Enum):
     Tecnico = 1
@@ -22,6 +25,7 @@ class TipoUsuario(Enum):
     GerenteDeVenta = 3
     Vendedor = 4
     SupervisorTecnico = 5
+
 
 lstProductos = conn.execute(productos.select()).fetchall()
 lstProforma = conn.execute(proforma.select()).fetchall()
@@ -34,22 +38,27 @@ lstDetFacturas = conn.execute(det_facturas.select()).fetchall()
 lstHorarioProgramacion = conn.execute(horario_programacion.select()).fetchall()
 
 load_dotenv()
-slack_token = os.getenv('SLACK_TOKEN')
+# slack_token = os.getenv('SLACK_TOKEN')
+# client = WebClient(token=slack_token)
 ssl._create_default_https_context = ssl._create_unverified_context
-client = WebClient(token=slack_token)
+webhook_programing = os.getenv('WEBHOOK_PROGRAMACION_URL')
+
 
 def create_google_calendar_event(horario_row, servicio, facturaobj, proformaobj, direccion, UrlGeoLocalizacion, observaciones, referencia):
     # Carga las credenciales de la cuenta de servicio
-    creds = Credentials.from_service_account_file('alaskacool-ee34eec8f111.json', 
+    creds = Credentials.from_service_account_file('alaskacool-ee34eec8f111.json',
                                                   scopes=['https://www.googleapis.com/auth/calendar'])
 
     # Usa las credenciales para acceder al servicio de Google Calendar
     service = build('calendar', 'v3', credentials=creds)
 
-    cliente = facturaobj.Nombrede if facturaobj else (proformaobj.Nombrede if proformaobj else "")
+    cliente = facturaobj.Nombrede if facturaobj else (
+        proformaobj.Nombrede if proformaobj else "")
 
-    start_datetime = horario_row.fechainicio.replace(hour=horario_row.horainicio.hour, minute=horario_row.horainicio.minute, second=horario_row.horainicio.second)
-    end_datetime = horario_row.fechafin.replace(hour=horario_row.horafin.hour, minute=horario_row.horafin.minute, second=horario_row.horafin.second)
+    start_datetime = horario_row.fechainicio.replace(
+        hour=horario_row.horainicio.hour, minute=horario_row.horainicio.minute, second=horario_row.horainicio.second)
+    end_datetime = horario_row.fechafin.replace(
+        hour=horario_row.horafin.hour, minute=horario_row.horafin.minute, second=horario_row.horafin.second)
 
     direccion = direccion if direccion else "N/A"
     UrlGeoLocalizacion = UrlGeoLocalizacion if UrlGeoLocalizacion else "N/A"
@@ -57,8 +66,10 @@ def create_google_calendar_event(horario_row, servicio, facturaobj, proformaobj,
 
     # Evento a crear
     event = {
-        'summary': cliente + ' - ' + servicio.descripcion + ' - Referencia: ' + referencia,  # Puedes personalizar este texto
-        'description':  f'Direccion: {direccion}\nLocalización: {UrlGeoLocalizacion}\nObservaciones: {observaciones}',  # Puedes personalizar este texto
+        # Puedes personalizar este texto
+        'summary': cliente + ' - ' + servicio.descripcion + ' - Referencia: ' + referencia,
+        # Puedes personalizar este texto
+        'description':  f'Direccion: {direccion}\nLocalización: {UrlGeoLocalizacion}\nObservaciones: {observaciones}',
         'start': {
             'dateTime': start_datetime.strftime('%Y-%m-%dT%H:%M:%S'),
             'timeZone': 'America/Managua',
@@ -72,32 +83,49 @@ def create_google_calendar_event(horario_row, servicio, facturaobj, proformaobj,
     calendar_id = 'bdb1c1dd54cb4991313cbcfda21f549b35e0d40f0103f06812e8dc86f5b20a91@group.calendar.google.com'
 
     # Usa el método insert del servicio de calendar para crear el evento
-    created_event = service.events().insert(calendarId=calendar_id, body=event).execute()
+    created_event = service.events().insert(
+        calendarId=calendar_id, body=event).execute()
     print(f"Evento calendario: {created_event['id']}")
     return created_event['id']  # Retorna el ID del evento creado
 
+
 def delete_google_calendar_event(event_id):
     # Carga las credenciales de la cuenta de servicio
-    creds = Credentials.from_service_account_file('alaskacool-ee34eec8f111.json', 
+    creds = Credentials.from_service_account_file('alaskacool-ee34eec8f111.json',
                                                   scopes=['https://www.googleapis.com/auth/calendar'])
 
     # Usa las credenciales para acceder al servicio de Google Calendar
     service = build('calendar', 'v3', credentials=creds)
-    
+
     calendar_id = 'bdb1c1dd54cb4991313cbcfda21f549b35e0d40f0103f06812e8dc86f5b20a91@group.calendar.google.com'
-    
+
     # Eliminar el evento
     service.events().delete(calendarId=calendar_id, eventId=event_id).execute()
 
 
 def send_message(text: str):
-    try:
-        response = client.chat_postMessage(
-            channel="C05L4FPK7CG",  # Aquí puedes cambiar el ID de tu canal
-            text=text)  # Aquí utilizamos la variable text
-    except SlackApiError as e:
-        print(f"Got an error: {e.response['error']}")
-        print(slack_token)
+    def main():
+        url = webhook_programing
+        app_message = {
+            'text': str
+        }
+        message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
+        http_obj = Http()
+        response = http_obj.request(
+            uri=url,
+            method='POST',
+            headers=message_headers,
+            body=dumps(app_message),
+        )
+        print(response)
+    # try:
+    #     response = client.chat_postMessage(
+    #         channel="C05L4FPK7CG",  # Aquí puedes cambiar el ID de tu canal
+    #         text=text)  # Aquí utilizamos la variable text
+    # except SlackApiError as e:
+    #     print(f"Got an error: {e.response['error']}")
+    #     print(slack_token)
+
 
 @strawberry.type
 class Productos:
@@ -247,6 +275,7 @@ class Productos:
     def from_row(cls, row):
         return cls(**row._asdict())
 
+
 @strawberry.type
 class Usuario:
     id: int
@@ -258,6 +287,7 @@ class Usuario:
     @classmethod
     def from_row(cls, row):
         return cls(**row._asdict())
+
 
 @strawberry.type
 class HorarioProgramacion:
@@ -274,20 +304,20 @@ class HorarioProgramacion:
 
 @strawberry.mutation
 async def crear_programacion(
-    self, 
-    codservicio: str, 
-    idUsuarioCreacion: Optional[int],
-    UrlGeoLocalizacion: str, 
-    info: Info,
-    codcliente: Optional[str] = None, 
-    codfactura: Optional[str] = None, 
-    codproforma: Optional[str] = None,
-    direccion: Optional[str] = None,
-    observaciones: Optional[str] = None,
-    idDepartamento: Optional[int] = None,
-    idHorarioProgramacion: Optional[int] = None,
-    idEstadoProgramacion: Optional[int] = None) -> int:
-    
+        self,
+        codservicio: str,
+        idUsuarioCreacion: Optional[int],
+        UrlGeoLocalizacion: str,
+        info: Info,
+        codcliente: Optional[str] = None,
+        codfactura: Optional[str] = None,
+        codproforma: Optional[str] = None,
+        direccion: Optional[str] = None,
+        observaciones: Optional[str] = None,
+        idDepartamento: Optional[int] = None,
+        idHorarioProgramacion: Optional[int] = None,
+        idEstadoProgramacion: Optional[int] = None) -> int:
+
     data_programacion = {
         "codservicio": codservicio,
         "idUsuarioCreacion": idUsuarioCreacion,
@@ -302,12 +332,14 @@ async def crear_programacion(
         "idHorarioProgramacion": idHorarioProgramacion
     }
     result = conn.execute(programacion.insert(), data_programacion)
-    
-    usuario_row = conn.execute(usuarios.select().where(usuarios.c.id == idUsuarioCreacion)).fetchone()
-    servicio_row = conn.execute(productos.select().where(productos.c.CodProducto == codservicio)).fetchone()
+
+    usuario_row = conn.execute(usuarios.select().where(
+        usuarios.c.id == idUsuarioCreacion)).fetchone()
+    servicio_row = conn.execute(productos.select().where(
+        productos.c.CodProducto == codservicio)).fetchone()
 
     servicio = Productos.from_row(servicio_row)
-    usuario = Usuario.from_row(usuario_row) 
+    usuario = Usuario.from_row(usuario_row)
 
     ref_value = codfactura if codfactura else codproforma
     id_value = result.inserted_primary_key[0]
@@ -319,37 +351,49 @@ async def crear_programacion(
     conn.commit()
     return int(result.inserted_primary_key[0])
 
+
 def get_usuario(idUsuarioActualizador):
-    usuario_row = conn.execute(usuarios.select().where(usuarios.c.id == idUsuarioActualizador)).fetchone()
+    usuario_row = conn.execute(usuarios.select().where(
+        usuarios.c.id == idUsuarioActualizador)).fetchone()
     return Usuario.from_row(usuario_row)
 
+
 def get_servicio(codservicio):
-    servicio_row = conn.execute(productos.select().where(productos.c.CodProducto == codservicio)).fetchone()
+    servicio_row = conn.execute(productos.select().where(
+        productos.c.CodProducto == codservicio)).fetchone()
     return Productos.from_row(servicio_row)
+
 
 def get_referencia(codfactura, codproforma):
     return codfactura if codfactura else codproforma
 
+
 def get_proforma_or_factura(referencia):
-    proformaobj = conn.execute(proforma.select().where(proforma.c.NoFactura == referencia)).fetchone()
+    proformaobj = conn.execute(proforma.select().where(
+        proforma.c.NoFactura == referencia)).fetchone()
     if not proformaobj:
         return None, conn.execute(facturas.select().where(facturas.c.NoFactura == referencia)).fetchone()
     return proformaobj, None
 
+
 def update_google_calendar_event(id, horario_row, servicio, facturaobj, proformaobj, direccion, UrlGeoLocalizacion, observaciones, referencia):
-    codeCalenderEvent = create_google_calendar_event(horario_row, servicio, facturaobj, proformaobj, direccion, UrlGeoLocalizacion, observaciones, referencia)
+    codeCalenderEvent = create_google_calendar_event(
+        horario_row, servicio, facturaobj, proformaobj, direccion, UrlGeoLocalizacion, observaciones, referencia)
     conn.execute(programacion.update().where(programacion.c.id == id), {
         "CodeGoogleCalendar": codeCalenderEvent
     })
+
 
 def notify_update(idUsuarioActualizador, text):
     if idUsuarioActualizador != 1:
         send_message(text)
 
+
 @strawberry.mutation
 def eliminar_programacion(self, id: int,
-    idUsuarioActualizador: Optional[int] = None) -> str:
-    event_row = conn.execute(programacion.select().where(programacion.c.id == id)).fetchone()
+                          idUsuarioActualizador: Optional[int] = None) -> str:
+    event_row = conn.execute(programacion.select().where(
+        programacion.c.id == id)).fetchone()
     usuario = get_usuario(idUsuarioActualizador)
     ref_value = get_referencia(event_row.codfactura, event_row.codproforma)
 
@@ -362,27 +406,28 @@ def eliminar_programacion(self, id: int,
 
     text = f"El usuario *{usuario.nombre}* ELIMINO programación con la referencia: *{ref_value}*. URL: https://alaska-cool-programacion.vercel.app/registerprograming/{id}"
     notify_update(idUsuarioActualizador, text)
-    
+
     conn.commit()
     return str(resultUpd.rowcount) + " Row(s) updated"
 
+
 @strawberry.mutation
-def actualizar_programacion(self, id: int, 
-    codservicio: str, 
-    idUsuarioCreacion: int, 
-    UrlGeoLocalizacion: str, 
-    info: Info,
-    codcliente: Optional[str] = None, 
-    codfactura: Optional[str] = None, 
-    codproforma: Optional[str] = None,
-    idCuadrilla: Optional[int] = None, 
-    idHorarioProgramacion: Optional[int] = None,
-    direccion: Optional[str] = None,
-    observaciones: Optional[str] = None,
-    idDepartamento: Optional[int] = None,
-    idEstadoProgramacion: Optional[int] = None,
-    idUsuarioActualizador: Optional[int] = None) -> str:
-    
+def actualizar_programacion(self, id: int,
+                            codservicio: str,
+                            idUsuarioCreacion: int,
+                            UrlGeoLocalizacion: str,
+                            info: Info,
+                            codcliente: Optional[str] = None,
+                            codfactura: Optional[str] = None,
+                            codproforma: Optional[str] = None,
+                            idCuadrilla: Optional[int] = None,
+                            idHorarioProgramacion: Optional[int] = None,
+                            direccion: Optional[str] = None,
+                            observaciones: Optional[str] = None,
+                            idDepartamento: Optional[int] = None,
+                            idEstadoProgramacion: Optional[int] = None,
+                            idUsuarioActualizador: Optional[int] = None) -> str:
+
     result = conn.execute(programacion.update().where(programacion.c.id == id), {
         "codservicio": codservicio,
         "idUsuarioCreacion": idUsuarioCreacion,
@@ -396,26 +441,29 @@ def actualizar_programacion(self, id: int,
         "observaciones": observaciones,
         "idDepartamento": idDepartamento,
         "idEstadoProgramacion": idEstadoProgramacion,
-        
+
     })
 
     usuario = get_usuario(idUsuarioActualizador)
     servicio = get_servicio(codservicio)
     referencia = get_referencia(codfactura, codproforma)
     proformaobj, facturaobj = get_proforma_or_factura(referencia)
-    
-    horario_row = conn.execute(horario_programacion.select().where(horario_programacion.c.id == idHorarioProgramacion)).fetchone()
-    
+
+    horario_row = conn.execute(horario_programacion.select().where(
+        horario_programacion.c.id == idHorarioProgramacion)).fetchone()
+
     if usuario.idTipoUsuario == TipoUsuario.SupervisorTecnico.value:
-        update_google_calendar_event(id, horario_row, servicio, facturaobj, proformaobj, direccion, UrlGeoLocalizacion, observaciones, referencia)
-    
+        update_google_calendar_event(id, horario_row, servicio, facturaobj,
+                                     proformaobj, direccion, UrlGeoLocalizacion, observaciones, referencia)
+
     ref_value = get_referencia(codfactura, referencia)
     text = f"El usuario *{usuario.nombre}* actualizo programación con la referencia: *{ref_value}*. URL: https://alaska-cool-programacion.vercel.app/registerprograming/{id}"
 
     notify_update(idUsuarioActualizador, text)
-    
+
     conn.commit()
     return str(result.rowcount) + " Row(s) updated"
-    
 
-lstProgramacionMutation = [crear_programacion, actualizar_programacion, eliminar_programacion]
+
+lstProgramacionMutation = [crear_programacion,
+                           actualizar_programacion, eliminar_programacion]
