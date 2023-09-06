7c1923a905d5480b982e6c7965391c9a4c5e639f
m272wsakoqfrw4ev2ilukcldmjkjdivxlwdadrlbpyyzyx4lx5ga <reko.image>
2023-09-05 22:45:37 -0600

    Update code

--- conn/db.py
+++ conn/db.py
@@ -15,9 +15,25 @@ engine = create_engine('mysql+pymysql://root:Carl1991*+1@34.173.53.131:3306/alas
 meta = MetaData()
 conn = engine.connect()
 
+# Conexión a SQL Server con pymssql
+# Nota: Asegúrate de cambiar YOUR_USERNAME y YOUR_PASSWORD por tus credenciales de SQL Server Authentication.
+# conn_sql = pyodbc.connect(
+#     "Driver={ODBC Driver 17 for SQL Server}",
+#     server="20.120.95.95\ALASKACOOL",
+#     user="LZepeda",
+#     password="Zepeda2023",
+#     database="2201ALASKACOOL_CENTRAL",
+#     schema="ALASKACOOL_Lectura",
+#     sslmode="no_verify"
+# )
+
+# connection_string = (
+#     "mssql+pyodbc://LZepeda:Zepeda2023@20.120.95.95\ALASKACOOL/2201ALASKACOOL_CENTRAL?"
+#     "driver=ODBC+Driver+17+for+SQL+Server&sslmode=no_verify"
+# )
+
 connection_string = (
-    "mssql+pyodbc://LZepeda:Zepeda2023@20.120.95.95\ALASKACOOL/2201ALASKACOOL_CENTRAL?"
-    "driver=ODBC+Driver+17+for+SQL+Server&sslmode=no_verify"
+    'mssql+pyodbc://LZepeda:Zepeda2023@20.120.95.95\ALASKACOOL/2201ALASKACOOL_CENTRAL?driver=ODBC+Driver+17+for+SQL+Server'
 )
 
 engine_sql = create_engine(connection_string, echo=True,
