569e83c2fbb0ef5c22f59be3b07f7da1614c32ad
m272wsakoqfrw4ev2ilukcldmjkjdivxlwdadrlbpyyzyx4lx5ga <reko.image>
2023-09-16 10:49:15 -0600

    Update code

--- queries/archivoprogramacion.py
+++ queries/archivoprogramacion.py
@@ -1,19 +1,33 @@
 import typing
 import strawberry
 from conn.db import conn
+from strawberry.types import Info
+from typing import Optional
 from models.index import archivo_programacion
+from models.index import tipo_archivo
+from .tipoarchivo import *
 
 @strawberry.type
 class ArchivoProgramacion:
     id: int
     PathArchivo: str
     idTipoArchivo: int
+    @strawberry.field
+    def tipoarchivo(self, info: Info) -> typing.List[Optional[TipoArchivo]]:
+        current_det_proformas = conn.execute(tipo_archivo.select()).fetchall()
+
+        matched_proformas = [
+            TipoArchivo(**dict(det._mapping))
+            for det in current_det_proformas 
+            if det.id == self.idTipoArchivo
+        ]
+        return matched_proformas or []
     idProgramacion: int
     NombreArchivo: str
 
 @strawberry.field
 def archivo_programacion_por_id(id: int) -> ArchivoProgramacion:
-    result = conn.execute(archivo_programacion.select().where(archivo_programacion.c.id == id)).fetchone()
+    result = conn.execute(archivo_programacion.select().where(archivo_programacion.c.idProgramacion == id)).fetchone()
     return result
 
 @strawberry.field
